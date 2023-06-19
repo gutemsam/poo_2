72eadd548ba724effa024d5aa94c5fff433563a4
Samuel Gutemberg Pereira <71667058+gutemsam@users.noreply.github.com>
2023-06-19 10:12:15 -0300

    Update main.dart
    deixando o codigo mais explicativo

--- projeto/main.dart
+++ projeto/main.dart
@@ -4,8 +4,10 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:solidart/solidart.dart';
 
+// Enumeração para representar o estado da tabela
 enum TableStatus { idle, loading, ready, error }
 
+// Classe responsável por carregar os dados e notificar as mudanças de estado da tabela
 class DataService {
   final ValueNotifier<Map<String, dynamic>> tableStateNotifier =
       ValueNotifier({
@@ -13,6 +15,7 @@ class DataService {
     'dataObjects': [],
   });
 
+  // Função para carregar os dados com base no índice fornecido
   void carregar(index) {
     final funcoes = [carregarComidas];
     tableStateNotifier.value = {
@@ -22,12 +25,13 @@ class DataService {
     funcoes[index]();
   }
 
+  // Função para carregar as refeições de frango da API
   void carregarComidas() {
     var mealsUri = Uri(
       scheme: 'https',
       host: 'www.themealdb.com',
       path: 'api/json/v1/1/search.php',
-      queryParameters: {'s': 'chicken'}, // Example search parameter for chicken meals
+      queryParameters: {'s': 'chicken'}, // Parâmetro de pesquisa de exemplo para refeições de frango
     );
 
     http.get(mealsUri).then((response) {
@@ -35,13 +39,14 @@ class DataService {
       tableStateNotifier.value = {
         'status': TableStatus.ready,
         'dataObjects': mealsJson['meals'],
-        'propertyNames': ["strMeal", "strCategory", "strArea"],
-        'columnNames': ["Name", "Category", "Area"],
+        'propertyNames': ["strMeal", "strCategory", "strArea"], // Nomes das propriedades a serem exibidas na lista
+        'columnNames': ["Name", "Category", "Area"], // Nomes das colunas da tabela
       };
     });
   }
 }
 
+// Instância do DataService para gerenciar os dados da tabela
 final dataService = DataService();
 
 void main() {
@@ -65,18 +70,18 @@ class MyApp extends StatelessWidget {
           builder: (_, value, __) {
             switch (value['status']) {
               case TableStatus.idle:
-                return Center(child: Text("Toque algum botão, abaixo..."));
+                return Center(child: Text("Toque algum botão, abaixo...")); // Mensagem exibida quando a tabela está inativa
               case TableStatus.loading:
-                return Center(child: CircularProgressIndicator());
+                return Center(child: CircularProgressIndicator()); // Indicador de carregamento exibido enquanto os dados estão sendo carregados
               case TableStatus.ready:
                 return ListWidget(
                   jsonObjects: value['dataObjects'],
                   propertyNames: value['propertyNames'],
-                );
+                ); // Exibe a lista de objetos quando os dados estão prontos
               case TableStatus.error:
-                return Text("Lascou");
+                return Text("Lascou"); // Mensagem de erro caso ocorra um erro no carregamento dos dados
             }
-            return Text("...");
+            return Text("..."); // Estado de fallback, caso nenhum dos casos anteriores seja correspondido
           },
         ),
         bottomNavigationBar: NewNavBar(itemSelectedCallback: dataService.carregar),
@@ -85,6 +90,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// Widget para exibir a barra de navegação inferior
 class NewNavBar extends HookWidget {
   final _itemSelectedCallback;
 
@@ -104,13 +110,14 @@ class NewNavBar extends HookWidget {
       items: const [
         BottomNavigationBarItem(
           label: "Comidas",
-          icon: Icon(Icons.restaurant_menu),
+          icon: Icon(Icons.restaurant_menu), // Ícone para o item "Comidas" na barra de navegação
         ),
       ],
     );
   }
 }
 
+// Widget para exibir a lista de objetos na tela
 class ListWidget extends StatelessWidget {
   final List jsonObjects;
   final List<String> propertyNames;
@@ -130,8 +137,8 @@ class ListWidget extends StatelessWidget {
       ),
       itemCount: jsonObjects.length,
       itemBuilder: (_, index) {
-        var title = jsonObjects[index][propertyNames[0]];
-        var content = propertyNames.sublist(1).map((prop) => jsonObjects[index][prop]).join(" - ");
+        var title = jsonObjects[index][propertyNames[0]]; // Obtém o título do objeto com base na primeira propriedade
+        var content = propertyNames.sublist(1).map((prop) => jsonObjects[index][prop]).join(" - "); // Obtém o conteúdo concatenando as propriedades restantes
 
         return Card(
           shadowColor: Theme.of(context).primaryColor,
@@ -141,8 +148,8 @@ class ListWidget extends StatelessWidget {
               Text(
                 "$title\n",
                 style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Text(content),
+              ), // Exibe o título em negrito
+              Text(content), // Exibe o conteúdo
               SizedBox(height: 10),
             ],
           ),
